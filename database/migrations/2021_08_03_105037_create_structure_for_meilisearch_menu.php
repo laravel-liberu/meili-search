5cebb63bdd5b05f5ed5544b3f4fcd91c5cef5bd5
Vlad Chiovianu <vlad.chiovianu@earthlink.ro>
2021-09-06 18:20:08 +0300

    fixes form structure, updates order index

--- database/migrations/2021_08_03_105037_create_structure_for_meilisearch_menu.php
+++ database/migrations/2021_08_03_105037_create_structure_for_meilisearch_menu.php
@@ -5,7 +5,7 @@ use LaravelEnso\Migrator\Database\Migration;
 class CreateStructureForMeilisearchMenu extends Migration
 {
     protected array $menu = [
-        'name' => 'MeiliSearch', 'icon' => 'fab search', 'route' => null, 'order_index' => 200, 'has_children' => true,
+        'name' => 'MeiliSearch', 'icon' => 'search', 'route' => null, 'order_index' => 600, 'has_children' => true,
     ];
 
     protected ?string $parentMenu = 'Integrations';
